ff07c83722609f46f9fb6982e163205ff778ba45
WaterMelony <vlad12345678934@gmail.com>
2023-06-06 21:54:10 +0300

    fix

--- test.js
+++
@@ -1,6 +0,0 @@
-var SingleBar = require('cli-progress').SingleBar;
-var bar = new SingleBar({});
-bar.start(500, 0);
-bar.update(100);
-bar.update(500);
-bar.stop();
--- test.ts
+++
@@ -1,8 +0,0 @@
-const {SingleBar} = require('cli-progress')
-
-const bar = new SingleBar({});
-
-bar.start(500, 0);
-bar.update(100)
-bar.update(500)
-bar.stop()
